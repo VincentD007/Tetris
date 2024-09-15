07fa80f732bdb3436bd26f9f47f53502aa4d1666
Vincent <vincentedward007@gmail.com>
2024-09-15 14:26:07 -0700

    Added rotational movement to Tpiece object

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ WIDTH = 700
 HEIGHT = 700
 SCREEN = pg.display.set_mode((WIDTH, HEIGHT))
 CLOCK = pg.time.Clock()
+PIECE_MOVE = pg.event.Event(pg.USEREVENT + 1)
 
 def draw_grid(screen):
     spacing = 25
@@ -26,18 +27,34 @@ def draw_grid(screen):
         spacing += 25
 
 
-vincent = Cube("green", 250, 50)
+vincent = Tpiece()
 
 def main():
+    piece_move = True
     playgame = True
     while playgame:
         CLOCK.tick(60)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 playgame = False
+            elif event == PIECE_MOVE:
+                piece_move = True
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    vincent.rotate()
+                elif event.key == pg.K_LEFT:
+                    vincent.move("left")
+                elif event.key == pg.K_RIGHT:
+                    vincent.move("right")
         SCREEN.fill((50, 50, 50))
+
         draw_grid(SCREEN)
+        if piece_move:
+            vincent.move("down")
+            piece_move = False
+            pg.time.set_timer(PIECE_MOVE, 1000)
         vincent.draw(SCREEN)
+
         pg.display.update()
     pg.quit()
 
--- tetris_objects.py
+++ tetris_objects.py
@@ -1,8 +1,6 @@
 import pygame as pg
 pg.init()
 
-
-
 purple_block = pg.transform.scale(pg.image.load("purple_block.png"), (25, 25))
 blue_block = pg.transform.scale(pg.image.load("blue_block.png"), (25, 25))
 gold_block = pg.transform.scale(pg.image.load("gold_block.png"), (25, 25))
@@ -30,7 +28,7 @@ class Cube:
         self.x, self.y = x_position, y_position
         self.rect = self.image.get_rect(topleft = (self.x, self.y))
 
-    
+ 
     def move(self):
         self.y += 25
     
@@ -41,36 +39,61 @@ class Cube:
 
 class Tpiece:
     def __init__(self) -> None:
-        self.leader_cube = Cube("purple", 375, 0)
+        self.reference_cube = Cube("purple", 350, 100)
         self.position = 1
-        self.positions = {}
 
 
-    def move(self):
-        self.leader_cube.y += 25
-        self.positions = {
-        1: [Cube("purple", self.leader_cube.x, self.leader_cube.y - 25), 
-            Cube("purple", self.leader_cube.x + 25, self.leader_cube.y),
-            Cube("purple", self.leader_cube.x - 25, self.leader_cube.y)],
+    def move(self, direction):
+        if direction == "down":
+            self.reference_cube.y += 25
+        elif direction == "left":
+            if self.position != 2:
+                if self.reference_cube.x > 250:
+                    self.reference_cube.x -= 25
+            else:
+                if self.reference_cube.x > 225:
+                    self.reference_cube.x -= 25
+        elif direction == "right":
+            if self.position != 4:
+                if self.reference_cube.x < 425:
+                    self.reference_cube.x += 25
+            else:
+                if self.reference_cube.x < 450:
+                    self.reference_cube.x += 25
 
-        2: [Cube("purple", self.leader_cube.x, self.leader_cube.y - 25), 
-            Cube("purple", self.leader_cube.x, self.leader_cube.y + 25), 
-            Cube("purple", self.leader_cube.x + 25, self.leader_cube.y)],
+    def draw(self, screen):
+        positions = {
+        1: [Cube("purple", self.reference_cube.x, self.reference_cube.y - 25), 
+            Cube("purple", self.reference_cube.x + 25, self.reference_cube.y),
+            Cube("purple", self.reference_cube.x - 25, self.reference_cube.y)],
 
-        3: [Cube("purple", self.leader_cube.x, self.leader_cube.y + 25), 
-            Cube("purple", self.leader_cube.x + 25, self.leader_cube.y), 
-            Cube("purple", self.leader_cube.x - 25, self.leader_cube.y)],
+        2: [Cube("purple", self.reference_cube.x, self.reference_cube.y - 25), 
+            Cube("purple", self.reference_cube.x, self.reference_cube.y + 25), 
+            Cube("purple", self.reference_cube.x + 25, self.reference_cube.y)],
 
-        4: [Cube("purple", self.leader_cube.x, self.leader_cube.y - 25), 
-            Cube("purple", self.leader_cube.x, self.leader_cube.y + 25), 
-            Cube("purple", self.leader_cube.x - 25, self.leader_cube.y)]
+        3: [Cube("purple", self.reference_cube.x, self.reference_cube.y + 25), 
+            Cube("purple", self.reference_cube.x + 25, self.reference_cube.y), 
+            Cube("purple", self.reference_cube.x - 25, self.reference_cube.y)],
+
+        4: [Cube("purple", self.reference_cube.x, self.reference_cube.y - 25), 
+            Cube("purple", self.reference_cube.x, self.reference_cube.y + 25), 
+            Cube("purple", self.reference_cube.x - 25, self.reference_cube.y)]
         }
 
-    
-    def draw(self, screen):
-        self.leader_cube.draw(screen)
-        for cube in self.position[self.position]:
+        self.reference_cube.draw(screen)
+        for cube in positions[self.position]:
             cube.draw(screen)
+
+
+    def rotate(self):
+        if self.reference_cube.x == 225:
+            self.reference_cube.x += 25
+        elif self.reference_cube.x == 450:
+            self.reference_cube.x -= 25
+        if self.position != 4:
+            self.position += 1
+        else:
+            self.position = 1
 
 
 class Map:
