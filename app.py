4f020ab3ed44ae6e05b3248071382a14acb2505c
Vincent <vincentedward007@gmail.com>
2024-10-08 22:02:09 -0700

    Started building a better row deletion animation; Moved the Draw_Grid function into the Map class as a method; Refactored app.py to accomodate for the new animation

--- app.py
+++ app.py
@@ -7,43 +7,40 @@ WIDTH = 700
 HEIGHT = 700
 SCREEN = pg.display.set_mode((WIDTH, HEIGHT))
 CLOCK = pg.time.Clock()
-PIECE_MOVE = pg.event.Event(pg.USEREVENT + 1)
-
-def draw_grid(screen):
-    spacing = 25
-    for _ in range(20):
-        if _ == 19:
-            pg.draw.rect(screen, (0, 0, 0), pg.rect.Rect(225, spacing - 1, 250, 8))
-        else:
-            pg.draw.rect(screen, (0, 0, 0), pg.rect.Rect(225, spacing - 1, 250, 2))
-        spacing += 25
-    spacing = 25
-    for _ in range(11):
-        if _ == 0:
-            pg.draw.rect(screen, (0, 0, 0), pg.rect.Rect(193 + spacing, 0, 8, 507))
-        elif _ == 10:
-            pg.draw.rect(screen, (0, 0, 0), pg.rect.Rect(199 + spacing, 0, 8, 507))
-        else:
-            pg.draw.rect(screen, (0, 0, 0), pg.rect.Rect(199 + spacing, 0, 2, 507))
-        spacing += 25
+PIECE_MOVEDOWN = pg.event.Event(pg.USEREVENT + 1)
 
 
 def main():
+    player_score = 0
     game_loose = False
-    piece_move_down = True
     playgame = True
-    tetrismap = TetrisMap()
+    tetrismap = TetrisMap(SCREEN)
 
     piece = random.choice([Tpiece("purple"), Ipiece("light_blue"), Jpiece("blue")])
+    pg.time.set_timer(PIECE_MOVEDOWN, 120)
     while playgame:
         CLOCK.tick(60)
-        for event in pg.event.get():
+        events = pg.event.get()
+        for event in events:
             if event.type == pg.QUIT:
                 playgame = False
-            elif not game_loose:
-                if event == PIECE_MOVE:
-                    piece_move_down = True
-                if event.type == pg.KEYDOWN:
+
+        SCREEN.fill((70, 70, 70))
+        tetrismap.draw()
+
+        if not game_loose:
+            for event in events:
+                if event == PIECE_MOVEDOWN:
+                    has_moved = piece.move(tetrismap, "down")
+                    if has_moved:
+                        pg.time.set_timer(PIECE_MOVEDOWN, 120)
+                    elif len(tetrismap.out_of_bounds) > 0:
+                        game_loose = True
+                    else:
+                        tetrismap.add(piece)
+                        player_score += tetrismap.delete_completed_rows()
+                        piece = random.choice([Tpiece("purple"), Ipiece("light_blue"), Jpiece("blue")])
+                elif event.type == pg.KEYDOWN:
                     if event.key == pg.K_SPACE: 
                         piece.rotate(tetrismap)
                     elif event.key == pg.K_LEFT:
@@ -51,30 +48,10 @@ def main():
                     elif event.key == pg.K_RIGHT:
                         piece.move(tetrismap, "right")
 
-        SCREEN.fill((70, 70, 70))
-        draw_grid(SCREEN)
-        tetrismap.drawcubes(SCREEN)
-
-        if piece_move_down:
-            piece_move_down = False 
-            moved = piece.move(tetrismap, "down")
-            if not moved:
-                tetrismap.add(piece)
-                if len(tetrismap.out_of_bounds) > 0:
-                    game_loose = True
-                else:
-                    piece = random.choice([Tpiece("purple"), Ipiece("light_blue"), Jpiece("blue")])
-            if not game_loose:
-                pg.time.set_timer(PIECE_MOVE, 120)
-
-        complete_rows = tetrismap.complete_rows()
-        for row in complete_rows:
-            del tetrismap.rows[row]
-            tetrismap.rows.insert(0, [])
         for row_index in range(0, 20):
             for cube in tetrismap[row_index]:
                 if cube.rect.y // 25 != row_index:
-                    cube.rect.y += 1
+                    cube.rect.y += 5
 
         piece.draw(SCREEN)
         pg.display.update()
